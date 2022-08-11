8c0936b1fd971acf456c85544ae890edff7dd746
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-08-11 18:59:27 +0300

    createRequest.js basic sourse is coded up but need to call this for request options proper adjustment

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,6 +3,15 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  console.log(`createRequest.js:`);
   console.log(options);
+  
+  const method = options.method;
+  const requestUrl = options.url;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(method, url);
+  xhr.responseType = 'json';
+  //xhr.send();
 };
  
